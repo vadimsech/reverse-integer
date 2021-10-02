c6c94380aec1123d0a72f966f37946783fbf403f
vadimsech <vadim.sech.07@mail.ru>
2021-10-02 13:17:35 +0300

    Создал приложение для переворота числа

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tmp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 90120;
+            int result = 0;
+            int temp;
+            while (i > 9 || i < -9)
+            {
+                temp = i % 10;
+                i /= 10;
+                result = result * 10 + temp;
+            }          
+            result = result * 10 + i;
+            Console.WriteLine(result);            
+        }
+    }
+}
